29c61487f001a3c5324af7b0a065444b697a6e1f
Brian Leung <brianleung329@gmail.com>
2016-12-08 10:10:55 -0500

    i think i got it to work?

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -10,6 +10,10 @@ public class Barcode implements Comparable<Barcode>{
 //               _zip and _checkDigit are initialized.
   public Barcode(String zip) {
       _zip = zip+"";
+      int fakeit = Integer.parseInt(zip)}
+      if (!(zip.length()==5)){
+	  throw new IllegalArgumentException();
+      }
       int getsum = 0;
       for (int i = 0;i<zip.length()-1;i++){
 	  getsum+=Character.getNumericValue(_zip.charAt(i));
@@ -26,7 +30,13 @@ public class Barcode implements Comparable<Barcode>{
 
 
 // postcondition: computes and returns the check sum for _zip
-  private int checkSum(){}
+  private int checkSum(){
+      int ret = 0;
+      for (int i = 0;i<5;i++){
+	  ret+=Character.getNumericValue(_zip.charAt(i));
+      }
+      return ret;
+  }
 
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
@@ -59,6 +69,17 @@ public class Barcode implements Comparable<Barcode>{
       return ret;
   }
 // postcondition: compares the zip + checkdigit, in numerical order. 
-  public int compareTo(Barcode other){}
+  public int compareTo(Barcode other){
+      String fir = _zip + _checkDigit;
+      int first = Integer.parseInt(fir);
+      int second = Integer.parseInt(other._zip + other._checkDigit);
+      if (first>second){
+	  return 1;
+      }else{
+	  if (second>first){
+	      return -1;}
+      }
+      return 0;
+  }
     
 }
