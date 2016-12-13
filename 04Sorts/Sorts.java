b50067d8eb5acab1cb4d43051b2c852c02b46a72
Brian Leung <brianleung329@gmail.com>
2016-12-13 15:36:17 -0500

    It dont compile yet, wait

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,13 @@
+public class Sorts{
+	public static String name(){
+		return "10.Leung.Brian";
+	}
+	public static void selectionSort(int[] data){
+		int meme = 0;
+		int whereto = 0;
+		for (int i = 0; i<data.length; i++){
+			if (data[i] < meme){
+				meme = data[i]
+			}
+	}
+}
