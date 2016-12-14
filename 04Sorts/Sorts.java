a75256189c73f7c7f5c70e795315b226e15ee626
Brian Leung <brianleung329@gmail.com>
2016-12-14 15:36:08 -0500

    Im gonna go get milk tea. By the way it still compiles.

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -3,11 +3,12 @@ public class Sorts{
 		return "10.Leung.Brian";
 	}
 	public static void selectionSort(int[] data){
-		int meme = 0;
+		int mem = 0;
 		int whereto = 0;
 		for (int i = 0; i<data.length; i++){
-			if (data[i] < meme){
-				meme = data[i]
+			if (data[i] < mem){
+				mem = data[i];
+				
 			}
 	}
 }
